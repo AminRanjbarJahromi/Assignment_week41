21c9b7fe0a83b2b2e32faeaf052b537e57a5ecd7
Amin Ranjbar <m.amin.ranjbar@gmail.com>
2022-10-14 22:19:27 +0200

    improve algorithm

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,49 @@
 package org.example;
 
-public class App 
-{
-    public static void main( String[] args ) {
+import java.util.Scanner;
 
+public class App {
+    public static void main(String[] args) {
+
+        while (true) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println(" Enter your number: ");
+            double n1 = scanner.nextInt();
+            System.out.println("Enter your operation type: + , - , * , /");
+            char operationType = scanner.next().charAt(0);
+            System.out.println(" Enter your number: ");
+            double n2 = scanner.nextInt();
+            switch (operationType) {
+                case '+':
+                    double res = n1 + n2;
+                    System.out.println(n1 + " + " + n2 + " = " + res);
+                    break;
+                case '-':
+                    double res1 = n1 - n2;
+                    System.out.println(n1 + " - " + n2 + " = " + res1);
+                    break;
+                case '*':
+                    double res3 = n1 * n2;
+                    System.out.println(n1 + " * " + n2 + " = " + res3);
+                    break;
+                case '/':
+                    double res4 = n1 / n2;
+                    System.out.println(n1 + " / " + n2 + " = " + res4);
+                    break;
+                default:
+                    System.out.println(" operation is not valid !");
+
+
+            }
+            System.out.println(" if you want to continue? yes or no : ");
+            String continueOperation = scanner.next();
+            if (continueOperation.equalsIgnoreCase("no")) {
+                //condition = false;
+                break;
+
+
+            }
+        }
     }
 }
--- src/main/java/org/example/Modules.java
+++ src/main/java/org/example/Modules.java
@@ -1,4 +1,41 @@
 package org.example;
 
 public class Modules {
+    public static double addition (double n1, double n2 ){
+
+        return n1+n2;
+    }
+
+    public static double subtraction(double n1, double n2, double n3, double n4, double n5,
+                                     double n6, double n7, double n8,double n9, double n10){
+
+        return n1-n2-n3-n4-n5-n6-n7-n8-n9-n10;
+
+    }
+
+    public static double multiplication(double n1, double n2, double n3, double n4, double n5,
+                                        double n6, double n7, double n8,double n9, double n10){
+
+        return n1*n2*n3*n4*n5*n6*n7*n8*n9*n10;
+    }
+    public static double division(double n1, double n2, double n3, double n4, double n5,
+                                  double n6, double n7, double n8,double n9, double n10){
+
+        return n1/n2/n3/n4/n5/n6/n7/n8/n9/n10;
+
+    }
+
+    public static void displayMenu (){
+        System.out.println("Please Enter Your Number : " );
+
+    }
+    public static void displayMenu1 (){
+        System.out.println("Please Enter Your Operation : ( + , - , * , / )" );
+    }
+
+
+
+
+
+
 }
