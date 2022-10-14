e881fba116a47cfd8fca4eb34d53c64cbb5b2358
Amin Ranjbar <m.amin.ranjbar@gmail.com>
2022-10-14 22:25:14 +0200

    improve algorithm

--- src/main/java/org/example/Modules.java
+++ src/main/java/org/example/Modules.java
@@ -6,22 +6,18 @@ public class Modules {
         return n1+n2;
     }
 
-    public static double subtraction(double n1, double n2, double n3, double n4, double n5,
-                                     double n6, double n7, double n8,double n9, double n10){
-
-        return n1-n2-n3-n4-n5-n6-n7-n8-n9-n10;
+    public static double subtraction(double n1, double n2){
+        return n1-n2;
 
     }
 
-    public static double multiplication(double n1, double n2, double n3, double n4, double n5,
-                                        double n6, double n7, double n8,double n9, double n10){
+    public static double multiplication(double n1, double n2){
 
-        return n1*n2*n3*n4*n5*n6*n7*n8*n9*n10;
+        return n1*n2;
     }
-    public static double division(double n1, double n2, double n3, double n4, double n5,
-                                  double n6, double n7, double n8,double n9, double n10){
+    public static double division(double n1, double n2){
 
-        return n1/n2/n3/n4/n5/n6/n7/n8/n9/n10;
+        return n1/n2;
 
     }
 
